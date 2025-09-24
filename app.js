0dbf53e6fdc9672083e786e9ae8e47ffe7f35f24
chijioke2603 <chijiokemarieclaire@gmail.com>
2025-09-24 11:29:34 +0100

    last modifications

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import chalk from "chalk";
 
 // importing custom modules
 import {Calculate} from "./calculator.js";
+import exponent from "./exponential.js";
 
 //my Registration Number
 console.log(chalk.cyan("Registration Number: BD/2025/TC3/028"));
@@ -21,17 +22,18 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
 });
+console.log(`below is a simple calculator for simple operations`);
    
 //requesting for numbers and completing operation
   rl.question("Please enter the first number: ",(num1)=>{
-  rl.question("Please enter second number: ",(num2) =>{
+  rl.question("Please enter second number: ", (num2) =>{
   rl.question("please choose operation (add,subtract,multiply,divide) ", (operation) => {
     let resulT;
     const calc =new Calculate(parseFloat(num1),parseFloat(num2));
   
    try{
     if (operation.toLowerCase() ==="add"){
-      resulT =chalk.bgBlue(`the sum is: ${calc.add(num1,num2)}`);
+      resulT =chalk.blue(`the sum is: ${calc.add(num1,num2)}`);
       console.log (resulT);
     }else if (operation.toLowerCase() ==="subtract"){
       resulT =chalk.bgRed(`the subtract is: ${calc.subtract(num1,num2)}`);
@@ -50,7 +52,15 @@ const rl = readline.createInterface({
     catch(error){
         resulT = chalk.red(`Error: ${error.message}`);
     } 
+
+ // this is my second module to print the exponent of numbers
+  console.log(chalk.bgBlueBright(`This will print out the exponent of each number you enter `));
+rl.question(chalk.green("Please enter the number: "),(num)=>{
+  rl.question(chalk.green("Please enter the exponent(power) "),(power) =>{
+    console.log (chalk.red`the answer is ${exponent(num,power)}`);
     rl.close();
+  });
+});
 });
 });
 });
---
+++ exponential.js
@@ -0,0 +1,6 @@
+// this module is used to calculate exponent of numbers
+export default function exponent(num, power){
+    let expo = num ** power;
+    return expo;
+
+}
